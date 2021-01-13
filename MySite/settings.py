fe4bf18d8e0d67cdf23da96cefc988b1052c80b7
Supriya67 <supriyaadhikary67@gmail.com>
2021-01-13 19:27:00 +0530

    procfile and settings

--- MySite/settings.py
+++ MySite/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'st#z7sys2i*65e^#61*i8rzzni5m99+x6t&zz*_udkom3jvnz%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','spicekafee.herokuapp.com']
 
 
 # Application definition
--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-web: gunicorn MySite.MySite.wsgi:application --log-file - --log-level debug
+web: gunicorn MySite.wsgi --log-file - --log-level debug
 python manage.py
 manage.py migrate
